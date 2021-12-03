31d517dbdabcf0eb8f04cb9eb4d3e59eb1a968a3
gashig007 <rustamgashig@gmail.com>
2021-12-03 21:38:17 +0600

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,31 +2,27 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(concat(10,20));
-        System.out.println(concat(20,30));
-        System.out.println(concat(30,40));
-        System.out.println(concat(40,50));
-        System.out.println(concat(45,55));
-    }
-    public static String concat(int age, int temperature){
-        String option = "Можно идти гулять";
-        String option1 = "Оставайтесь дома";
-        if(age > 20 && age < 45 && temperature > -20 && temperature < 30) {
-            return option;
-        }
+    public static String option = "Можно идти гулять";
+    public static String option1 = "Оставайтесь дома";
 
-        else if(age < 20 && temperature > 0 && temperature < 28){
+    public static void main(String[] args) {
+        System.out.println(concat(10, 20));
+        System.out.println(concat(20, 30));
+        System.out.println(concat(30, 40));
+        System.out.println(concat(40, 50));
+        System.out.println(concat(45, 55));
+    }
+
+    public static String concat(int age, int temperature) {
+        if (age > 20 && age < 45 && temperature > -20 && temperature < 30) {
             return option;
-        }
-        if(age > 45 && temperature > -10 && temperature < 25){
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
             return option;
-        }
-        else{
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
+            return option;
+        } else {
             return option1;
         }
-
-
     }
 }
 
